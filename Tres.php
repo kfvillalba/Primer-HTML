3a4ad88bb3f14d8313cc4af94c55269e7062c648
kfvillalba <70710723+kfvillalba@users.noreply.github.com>
2022-03-22 08:32:30 -0500

    php

---
+++ Tres.php
@@ -0,0 +1,29 @@
+<!DOCTYPE ph>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Pagina Dos</title>
+    <link rel="stylesheet" href="Estilo.css">
+</head>
+<body>
+
+
+    <table>
+        <tr>
+            <th>Nombre</th>
+            <th>Apellido</th>
+            <th>Direccion</th>
+            <th>Telefono</th>
+        </tr>
+
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+
+        </tr>
+
+    </table>
+</body>
+</html>
